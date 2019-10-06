da348dfd8e10120af7340e7e113b3d7109a76a8a
developerjigr <jigar.polra@icloud.com>
2019-10-06 23:37:15 +0100

    Updated main summary view controller

--- syncmoneytechnical/View Controllers/UserAccountSummaryViewController.swift
+++ syncmoneytechnical/View Controllers/UserAccountSummaryViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class UserAccountSummaryViewController: UIViewController {
 
+	//Should ideally be a table view with collection view nibs as cells
+	@IBOutlet var scrollView: UIScrollView!
+	@IBOutlet var headerViewContainer: UIView!
+	@IBOutlet var syncAccountCollectionViewContainer: UIView!
+	@IBOutlet var bankAccountCollectionViewContainer: UIView!
+
+	weak var summaryHeaderView: UserSummaryHeaderView?
+	weak var syncAccountCollection: UserAccountSummaryCollectionView?
+	weak var bankAccountCollection: UserAccountSummaryCollectionView?
+
 	var viewModel: UserAccountSummaryViewModel? {
 		didSet {
 			viewModel?.delegate = self
@@ -19,8 +29,32 @@ class UserAccountSummaryViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		setupSyncAccountCollectionView()
 	}
 
+	func setupSyncAccountCollectionView() {
+		guard
+			let headerViewNib = UserSummaryHeaderView.loadFromNib(),
+			let syncCollectionViewNib = UserAccountSummaryCollectionView.loadFromNib(),
+			let bankCollectionViewNib = UserAccountSummaryCollectionView.loadFromNib()
+		else {
+			fatalError("Problem loading UserAccountSummaryCollectionView nib")
+		}
+
+
+		headerViewNib.configureView()
+		syncCollectionViewNib.configureView(for: .syncAccount)
+		bankCollectionViewNib.configureView(for: .bankAccount)
+
+		headerViewNib.addToSubviewAndEmbed(into: headerViewContainer)
+		syncCollectionViewNib.addToSubviewAndEmbed(into: syncAccountCollectionViewContainer)
+		bankCollectionViewNib.addToSubviewAndEmbed(into: bankAccountCollectionViewContainer)
+
+		summaryHeaderView = headerViewNib
+		syncAccountCollection = syncCollectionViewNib
+		bankAccountCollection = bankCollectionViewNib
+
+	}
 
 
 }
@@ -28,20 +62,18 @@ class UserAccountSummaryViewController: UIViewController {
 extension UserAccountSummaryViewController: UserAccountSummaryDelegate {
 
 	func accountDidLoad(_ viewModel: UserAccountSummaryViewModel, syncAccountViewData: [SyncAccountViewData]) {
-
+		// pass in syncViewData
+		syncAccountCollection?.configureView(for: .syncAccount)
 	}
 
 	func accountDidLoad(_ viewModel: UserAccountSummaryViewModel, bankAccountViewData: [BankAccountViewData]) {
-
+		// pass in bankViewData
+		bankAccountCollection?.configureView(for: .bankAccount)
 	}
-
 
 	func didSelectAccount(_ viewModel: UserAccountSummaryViewModel, atIndexPath: IndexPath) {
 
 	}
-
-
-
 
 }
 
@@ -54,8 +86,8 @@ extension UserAccountSummaryViewController {
 		guard
 			let userAccountDetailViewController = segue.destination as? UserAccountDetailViewController,
 			let selectedAccountModel = viewModel?.selectedModel
-			else {
-				return
+		else {
+			return
 		}
 
 		let selectedAccountDetailViewModel = UserAccountDetailViewModel(
