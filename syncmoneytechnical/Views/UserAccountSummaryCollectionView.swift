1b79122f84da69831f5c0dc893661c8c9ec1a305
developerjigr <jigar.polra@icloud.com>
2019-10-06 16:30:42 +0100

    Created UserAccountCollection View

---
+++ syncmoneytechnical/Views/UserAccountSummaryCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  UserAccountSummaryCollectionView.swift
+//  syncmoneytechnical
+//
+//  Created by Jigar Polra on 06/10/2019.
+//  Copyright © 2019 JIG_R. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserAccountSummaryCollectionView: UIView {
+
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+	}
+}
