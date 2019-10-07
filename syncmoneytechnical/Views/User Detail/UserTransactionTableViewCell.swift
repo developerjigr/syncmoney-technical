e27f480b79c4b8d06b75bf4019057582f4ffb824
developerjigr <jigar.polra@icloud.com>
2019-10-07 20:47:15 +0100

    Created User Transaction table view cell nib

---
+++ syncmoneytechnical/Views/User Detail/UserTransactionTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  UserTransactionTableViewCell.swift
+//  syncmoneytechnical
+//
+//  Created by Jigar Polra on 07/10/2019.
+//  Copyright © 2019 JIG_R. All rights reserved.
+//
+
+import Foundation
